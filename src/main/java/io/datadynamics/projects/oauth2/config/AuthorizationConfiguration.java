f381257c4590d93a9563e3852d2ad4bc90fb134c
김병곤 <fharenheit@gmail.com>
2021-05-18 20:56:53 +0900

    인증 관련 추가

--- src/main/java/io/datadynamics/projects/oauth2/config/AuthorizationConfiguration.java
+++ src/main/java/io/datadynamics/projects/oauth2/config/AuthorizationConfiguration.java
@@ -1,22 +1,29 @@
 package io.datadynamics.projects.oauth2.config;
 
+import io.datadynamics.projects.oauth2.auth.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationConfiguration extends AuthorizationSErverConfigurerAdapter {
+public class AuthorizationConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     DataSource dataSource;
@@ -24,51 +31,53 @@ public class AuthorizationConfiguration extends AuthorizationSErverConfigurerAda
     @Value("${app.oauth2.jwt-signing-key}")
     String jwtSigningKey;
 
-    @Value
+    @Value("${app.oauth2.access-token-validaty-seconds}")
     int accessTokenValidatySeconds;
-    
+
     @Autowired
     @Qualifier("authenticationManagerBean")
     AuthenticationManager authenticationManager;
 
-    @Autowiared
-    UserDetailService userDetailSErvice;
+    @Autowired
+    CustomUserDetailService userDetailService;
 
     @Override
-    configure(AuthorizationServerEndpintsConfigurer endpoints) {
-        endpoints.tokenStore(tokenStore())
-            .approvalStore(approvalStore())
-            .acessTOkenConverter(...) // added for JWT
-            .authenticationManager(...)
-            userdetailservice(...);
-    
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
+
     @Override
-    
-    configure(CLientDetailConfiguerer lists) {
-        clients.jdbc(dataSOurce);
-    
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore())
+                .approvalStore(approvalStore())
+                .accessTokenConverter(accessTokenConverter()) // added for JWT
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailService);
+    }
+
     @Bean
     ApprovalStore approvalStore() {
         return new JdbcApprovalStore(dataSource);
     }
 
     @Bean
-    TokenStore tokenStore() {
-        return new JwtTokenStore(accessToken);
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
     }
-    
+
     @Bean
-    JwtAccessTokenConverter acessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAcessTokenConverter();
-        converter.setSigningKey(jwt);
-        return converter
-        }
-    
-    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtSigningKey);
+        return converter;
+    }
+
     @Primary
-    DefaultTokenService tokenService() {
-        DefaultTokenSErvice tokenService = new DefaultTokenSErvice()
-            toeknSErvice.setToeknStore(tokenStore());
-        return toeknService;
+    @Bean
+    public DefaultTokenServices tokenService() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore());
+        return tokenServices;
+    }
 
 }
--- src/main/java/io/datadynamics/projects/oauth2/config/ResourceServerConfiguration.java
+++ src/main/java/io/datadynamics/projects/oauth2/config/ResourceServerConfiguration.java
@@ -1,18 +1,28 @@
-@Congiruation
-@EnableREsourceSErver
-ResourceServerConfiguration extens ResourceServerConfigurerAdapter {
+package io.datadynamics.projects.oauth2.config;
 
-@Override
-configure(HttpSeurity http) {
-http.csrf().disable()
-.sessionManagement().sessionCreationPolicy(SessionCrationPolicy.NEVER)
-.and()
-.authroizedRequestS()
-.antMatchers(/sercice/**)
-.anyRequest().authenticatd()
-.and().logout()
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
-@Override
-configure(ResoruceServerSecurityConfigurere resources) {
-super.configure(resorues);
+@Configuration
+@EnableReactiveMethodSecurity
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        super.configure(resources);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER.NEVER)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/services/**");
+        http.authorizeRequests().anyRequest().authenticated().and().logout();
+    }
 }
